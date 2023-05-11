20dfe963d2229c4591aa90d0aefae7abb183066d
Volodymyr <volodyaruzhak@gmail.com>
2023-05-11 14:18:20 -0400

    init for full test - project

---
+++ src/components/InputCheckbox/index.tsx
@@ -0,0 +1,29 @@
+import classNames from "classnames"
+import { useRef } from "react"
+import { InputCheckboxComponent } from "./types"
+
+export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, disabled, onChange }) => {
+  const { current: inputId } = useRef(`RampInputCheckbox-${id}`)
+
+  return (
+    <div className="RampInputCheckbox--container" data-testid={inputId}>
+      <label
+        className={classNames("RampInputCheckbox--label", {
+          "RampInputCheckbox--label-checked": checked,
+          "RampInputCheckbox--label-disabled": disabled,
+        })}
+        //add htmlFor so input and label will know what its for
+        //could close label after input tag - also would work
+        htmlFor={inputId}
+      />
+      <input
+        id={inputId}
+        type="checkbox"
+        className="RampInputCheckbox--input"
+        checked={checked}
+        disabled={disabled}
+        onChange={() => onChange(!checked)}
+      />
+    </div>
+  )
+}
---
+++ src/components/Transactions/TransactionPane.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react"
+import { InputCheckbox } from "../InputCheckbox"
+import { TransactionPaneComponent } from "./types"
+//import this to get clearCache
+import { useCustomFetch } from "../../hooks/useCustomFetch"
+
+export const TransactionPane: TransactionPaneComponent = ({
+  transaction,
+  loading,
+  setTransactionApproval: consumerSetTransactionApproval,
+}) => {
+  const [approved, setApproved] = useState(transaction.approved)
+  // using clearCache from customFetch
+  const { clearCache } = useCustomFetch()
+
+  return (
+    <div className="RampPane">
+      <div className="RampPane--content">
+        <p className="RampText">{transaction.merchant} </p>
+        <b>{moneyFormatter.format(transaction.amount)}</b>
+        <p className="RampText--hushed RampText--s">
+          {transaction.employee.firstName} {transaction.employee.lastName} - {transaction.date}
+        </p>
+      </div>
+      <InputCheckbox
+        id={transaction.id}
+        checked={approved}
+        disabled={loading}
+        onChange={async (newValue) => {
+          await consumerSetTransactionApproval({
+            transactionId: transaction.id,
+            newValue,
+          })
+          //using clearCache so it will not stuck with checked or unchecked
+          clearCache()
+          setApproved(newValue)
+        }}
+      />
+    </div>
+  )
+}
+
+const moneyFormatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+})
